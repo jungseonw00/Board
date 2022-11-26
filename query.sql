7d648a2deebf78c3ba1005edb99ee74ace837969
jungseonwoo <laborlawseon@gmail.com>
2022-11-26 16:55:40 +0900

    - DB정보 Jasypt로 암호화하기 - 로그인 페이지 만들기

--- query.sql
+++ query.sql
@@ -12,13 +12,13 @@ create table board
 ) comment '게시판';
 
 create table user (
-      user_id varchar(50) not null primary key comment '아이디',
-      password varchar(50) not null comment '비밀번호',
-      name varchar(50) not null comment '별명',
-      email varchar(50) not null comment '이메일',
-      phone varchar(50) not null comment '휴대폰 번호',
-      address varchar(50) not null comment '주소',
+      user_id        varchar(50) not null primary key comment '아이디',
+      password       varchar(50) not null comment '비밀번호',
+      name           varchar(50) not null comment '별명',
+      email          varchar(50) not null comment '이메일',
+      phone          varchar(50) not null comment '휴대폰 번호',
+      address        varchar(50) not null comment '주소',
       detail_address varchar(50) not null comment '상세주소',
-      delete_at varchar(50) not null comment '삭제여부',
-      created_date datetime comment '생성날짜'
-);
+      delete_at      varchar(50) not null comment '삭제여부',
+      created_date   datetime comment '생성날짜'
+) comment '회원정보';
