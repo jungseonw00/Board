895b78da4c2a31e58ce7831beff5c72f68947b9d
정선우 <laborlawseon@gmail.com>
2023-05-12 22:49:06 +0900

    - MyBatis 삭제 - CSS 삭제

--- src/main/java/com/study/home/controller/HomeApiController.java
+++ src/main/java/com/study/home/controller/HomeApiController.java
@@ -1,11 +1,9 @@
 package com.study.home.controller;
 
-import com.study.home.dto.User;
 import com.study.home.model.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +14,7 @@ public class HomeApiController {
     private final UserService userService;
 
     @PostMapping("/duplicateId")
-    public String duplicateId(@RequestBody User params) {
+    public String duplicateId() {
         return "ok";
     }
 }
